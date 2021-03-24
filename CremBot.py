124e1951fd0c3cd3b0d12d5d4edc110ffabea053
ldelreal <74260806+ldelreal@users.noreply.github.com>
2021-03-24 16:21:25 -0500

    Update CremBot.py

--- CremBot.py
+++ CremBot.py
@@ -9,57 +9,12 @@ token = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 
-@bot.command(name='stick', help='Responds with Stick quotes from Stromlight Archive')
-async def stick(ctx):
-    i_am_stick_quotes = [
-        (
-            'Why don’t you become fire instead? :fire:'
-            '\n'
-            '\nI am a stick.:wood:'
-        ),
-        (
-            'You want to burn? :fire:'
-            '\n'
-            '\nI am a stick.:wood:'
-        ),
-        (
-            'Think of how much fun it would be? :tada:'
-            '\n'
-            '\nI am a stick.:wood:'
-        ),
-        'I am a stick.:wood:'
+@bot.command(name='any', help='Responds with '' quotes from '')
+async def 'name'(ctx):
+    quotes = ['array of quotes/text file/etc'
     ]
 
-    response = random.choice(i_am_stick_quotes)
-    await ctx.send(response)
-
-
-@bot.command(name='pattern', help='Responds with Pattern quotes from Stromlight Archive')
-async def pattern(ctx):
-    pattern_quotes = [
-        (
-            'Inappropriate?'
-            '\nSuch as...'
-            '\nDividing by zero? :zero:'
-        ),
-        (
-            'No mating.'
-            '\n'
-            '\nNO MATING.'
-            '\n'
-            '\nNO MATING!'
-
-        ),
-        (
-            'You were talking about mating! '
-            '\n'
-            '\nI’m to make sure you don’t accidentally mate.'
-        ),
-        'Terrible destruction to eat!:meat_on_bone:',
-        'Yes, yes. Mmmm.'
-    ]
-
-    response = random.choice(pattern_quotes)
+    response = random.choice(quotes)
     await ctx.send(response)
 
 bot.run(token)
